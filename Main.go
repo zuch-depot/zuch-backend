f99ca448db5b993d0a601e1ac16d7938bf7fef0f
Jannis <=>
2025-09-18 19:43:27 +0200

    User Tickers for Ticks

--- Main.go
+++ Main.go
@@ -35,14 +35,15 @@ func main() {
 	go startServer()
 
 	//Zeit pro Tick bestimmen
-	tickTime, err := strconv.ParseInt(os.Getenv("TICKTIMEMILISEC"), 10, 64)
+	ticksMilisec, err := strconv.Atoi(os.Getenv("TICKTIMEMILISEC"))
+
+	ticker := time.NewTicker(time.Duration(ticksMilisec) * time.Millisecond)
 	if err != nil {
 		logger.Error("Failed to convert Ticktime to Int", slog.String("Error", err.Error())) //anderes Log?// Panic beendet das programm :(
 	}
 
 	//jeder Tick
 	for tick := 0; ; tick++ {
-		start := time.Now()
 
 		//Speichern, welche Tiles am Ende des Threads entblocked werden muss
 		var tilesToUnblock []Tile
@@ -63,17 +64,10 @@ func main() {
 		}
 
 		//anzeigen Testing
-		if tick%500 == 0 {
+		if tick%50 == 0 {
 			printMap()
 		}
-
-		//syncen, dass jeder Tick nur 1 mal
-		remainingTime := tickTime - time.Since(start).Abs().Milliseconds()
-		if remainingTime < 1 {
-			//logging, dass der Server hinterher hinkt
-		} else {
-			time.Sleep(time.Duration(remainingTime * 1000))
-		}
+		<-ticker.C
 	}
 }
 
