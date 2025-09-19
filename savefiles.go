a1187ab9663146cca7f3fc2aa590479d5df1e344
Jannis <jannis@yaat.eu>
2025-09-20 01:25:10 +0200

    Beim speichern kann per env gesteuert werden ob compressed oder nicht, dateiname hat jetz uhrzeit

--- savefiles.go
+++ savefiles.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"log/slog"
 	"os"
+	"time"
 )
 
 // Speichert den Spielstand
@@ -32,26 +33,40 @@ func saveGame(users []*User,
 	// Einzelnes Object das hoffentlich den ganzen status des Spiels darstellt
 	state := gamestate{Users: users, Schedules: schedules, Stations: stations, Tiles: tiles, Trains: trains}
 	// Die Objekte werden in einer netten JSON verpackt
+
+	// Dateiname wird ggf. abgeändert wenn es nicht compressed wird
+	filename := os.Getenv("SAVELOCATION") + "/savegame-" + time.Now().Format("20060102-150405")
+	var bytesToWrite []byte
+
 	stateByte, err := json.Marshal(state)
 	if err != nil {
-		logger.Error("Could not save Json", slog.String("Error", err.Error()))
+		logger.Error("COuld not convert state to JSON", slog.String("Error", err.Error()))
 	}
 
-	// Damit die nicht so riesig wird, wird noch mit gzip das ganze etwas komprimiert
-	var buf bytes.Buffer
-	zw := gzip.NewWriter(&buf)
-	zw.Write(stateByte)
+	if os.Getenv("SAVECOMPRESSED") == "True" {
 
+		// Damit die nicht so riesig wird, wird noch mit gzip das ganze etwas komprimiert
+		var buf bytes.Buffer
+		zw := gzip.NewWriter(&buf)
+		zw.Write(stateByte)
+		bytesToWrite = stateByte
+	} else {
+		// falls man die sich selber mal anschauen will
+		filename += ".json"
+		bytesToWrite = stateByte
+	}
 	// dann wird es auf die Festplatte geschrieben
 	// Hier vielleicht noch die zeit oder das datum ans ende des dateinamen
-	err = os.WriteFile(os.Getenv("SAVELOCATION"), buf.Bytes(), 04)
+	logger.Info("Writing gamestate to Disk", slog.String("Filename", filename))
+	err = os.WriteFile(filename, bytesToWrite, 0644)
+
 	if err != nil {
 		logger.Error("Failure while writing File", slog.String("Error", err.Error()))
 	}
 }
 
 type gamestate struct {
-	Users     []User
+	Users     []*User
 	Schedules []Schedule
 	Stations  []Station
 	Tiles     [][]Tile
